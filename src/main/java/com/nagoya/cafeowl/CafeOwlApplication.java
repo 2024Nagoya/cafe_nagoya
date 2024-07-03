482a862846a67301f1dae8f5cbfb53d9b7377c74
N-jongsik <jongsik0625@naver.com>
2024-07-04 04:28:37 +0900

    first commit

---
+++ src/main/java/com/nagoya/cafeowl/CafeOwlApplication.java
@@ -0,0 +1,13 @@
+package com.nagoya.cafeowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOwlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeOwlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nagoya/cafeowl/controller/ReactRestController.java
@@ -0,0 +1,25 @@
+package com.nagoya.cafeowl.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class ReactRestController {
+
+    @PostMapping(value="/testData")
+    public Map<Integer, String> testData(@RequestBody List<String> params){
+        Map<Integer, String> data = new HashMap<>();
+        data.put(1,"사과");
+        data.put(2,"포도");
+        data.put(3,"바나나");
+
+        int i = 4;
+        for(String param : params){
+            data.put(i, param);
+            i++;
+        }
+
+        return data;
+    }
+}

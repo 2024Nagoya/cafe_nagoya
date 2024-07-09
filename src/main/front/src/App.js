cb3e02436650ad66185a1278e48da676cfc4980c
N-jongsik <jongsik0625@naver.com>
2024-07-10 01:18:21 +0900

    CafeSearch page

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainScreenForm from './components/MainScreenForm';
-import SignUpForm from './components/SignUpForm'; // SignUpForm 컴포넌트의 경로를 정확하게 지정해야 합니다.
+import SignUpForm from './components/SignUpForm';
+import CafeSearch from "./components/CafeSearch"; // SignUpForm 컴포넌트의 경로를 정확하게 지정해야 합니다.
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
            <Routes>
                <Route path="/" element={<MainScreenForm />} />
                <Route path="/api/auth/signup" element={<SignUpForm />} />
+               <Route path="/CafeSearch" element={<CafeSearch />} />
            </Routes>
        </Router>
     );
